f809e39acac54e70e3004bafb44d8f04a9e59a3f
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-14 01:56:05 +0100

    just hello world react app is imported with project notes.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1> Hellow Worl!</h1>;
+console.log(element);
+
+ReactDOM.render(element, document.getElementById('root'));
