51833c3ac816d3a3ebde648652eb4c2453de4980
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 16:34:39 +0100

    just some notes are added and corrected.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1> Hellow Worl!</h1>;
+const element = <h1> Hellow World!</h1>;
 console.log(element);
 
 ReactDOM.render(element, document.getElementById('root'));
