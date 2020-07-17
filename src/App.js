8df0645adbdebc57657e9e9e5fa77e89ac1557c8
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 21:43:08 +0100

    counting is updated in navbar.jsx

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ handleDelete = (counterId) =>{
   render () {
     return (
     <React.Fragment>
-      <NavBar/>
+      <NavBar totalCounters={this.state.counters.filter(c=> c.value > 0).length}/>
       <main className="container">
         <Counters
           counters={this.state.counters}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,9 @@ class NavBar extends Component {
     render() { 
         return ( 
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Navbar{" "}
+                    <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                </a>
             </nav>
          );
     }
