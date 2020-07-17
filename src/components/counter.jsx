901e3c09d473d3233cc736b12c6aaac4f3ba792f
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 17:56:19 +0100

    how to present a list using map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {  
-        count: 0
+        count: 0,
+        tags : ['tag1', 'tag2', 'tag3']
     }
     styles = {
         fontSize: 10,
@@ -11,10 +12,20 @@ class Counter extends Component {
     render() { 
         return (
             <div>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className={this.getBadgeClass()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
+                </ul>
             </div>
         );
+    }
+
+
+    getBadgeClass() {
+        let zclasses = 'badge badge-';
+        zclasses += (this.state.count == 0 ? 'warning' : 'primary');
+        return zclasses;
     }
 
     formatCount(){
