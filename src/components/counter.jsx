d872f6df8658732e0290047379e169f1e6fdb35c
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 17:45:16 +0100

    how to use styles in counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,18 +3,23 @@ class Counter extends Component {
     state = {  
         count: 0
     }
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
+
     render() { 
         return (
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
 
     formatCount(){
         const { count } = this.state;
-        const z = <h1>Zero</h1>;
+        const z = <h5>Zero</h5>;
         return count === 0 ? z : count;
     }
 
