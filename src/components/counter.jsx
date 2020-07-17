8eab179ec716d3b43b1357436f5fea75218f89b3
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 17:40:28 +0100

    just shows how classname can be used in React.createElement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     state = {  
-        count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        count: 0
     }
     render() { 
         return (
             <div>
-                <img src={this.state.imageUrl} alt=""/>
-                <span>{this.formatCount()}</span>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button>Increment</button>
             </div>
         );
