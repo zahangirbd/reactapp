7855843d294acefee408af63c8eaf912ac5afeea
Zahangir Alam <zahangirbd@hotmail.com>
2020-07-17 20:12:46 +0100

    increment and delete is handled in parent class using props instead of using child object.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-    state = {  
-        value: this.props.counter.value
-    }
     styles = {
         fontSize: 10,
         fontWeight: 'bold'
@@ -14,20 +11,13 @@ class Counter extends Component {
     //    this.handleIncrement = this.handleIncrement.bind(this); 
     //}
 
-    handleIncrement = (product) => {
-        console.log(product);
-        //we need to tell React that state has bee updated by a property.
-        this.setState({value: this.state.value + 1}); 
-    }
-
     render() { 
-        console.log("props", this.props);
         return (
             <div>
                 <h4>Counter #{this.props.counter.id}</h4>
                 <span style={this.styles} className={this.getBadgeClass()}>{this.formatCount()}</span>
                 <button 
-                    onClick={()=> this.handleIncrement({param_id:1})}
+                    onClick={() => this.props.onIncrement(this.props.counter)}
                     className="btn btn-secondary btn-sm">Increment
                 </button>
                 <button className="btn btn-danger btn-sm m-2" 
@@ -39,12 +29,12 @@ class Counter extends Component {
 
     getBadgeClass() {
         let zclasses = 'badge badge-';
-        zclasses += (this.state.value === 0 ? 'warning' : 'primary');
+        zclasses += (this.props.counter.value === 0 ? 'warning' : 'primary');
         return zclasses;
     }
 
     formatCount(){
-        const { value } = this.state;
+        const { value } = this.props.counter;
         const z = <h5>Zero</h5>;
         return value === 0 ? z : value;
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -12,6 +12,25 @@ class Counters extends Component {
         ]
     }
 
+    handleReset = ()=> {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+    }
+
+    handleIncrement = (counter) => {
+        //clone the counters here. Using ... we can clone object 
+        const counters = [...this.state.counters];
+        //find the right counter
+        const index = counters.indexOf(counter);
+        //clone the found counter
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({counters});
+    }
+
     handleDelete = (counterId) =>{
         console.log("Delete is invoked, id=", counterId);
         const counters = this.state.counters.filter(c=> c.id !== counterId);
@@ -22,11 +41,13 @@ class Counters extends Component {
     render() { 
         return ( 
             <div>
+                <button className="btn btn-primary btn-sm m-2" onClick={this.handleReset}>Reset</button>
                 {this.state.counters.map(
                     counter =>
                         <Counter 
                             key={counter.id}  
                             onDelete={this.handleDelete}
+                            onIncrement={this.handleIncrement}
                             counter={counter}
                         />
                         
